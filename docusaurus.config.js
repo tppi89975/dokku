9022d80d8b4516ca271c167f98f8bf93f380a41b
Martim Gomes <a89975@ualg.pt>
2025-11-24 16:46:44 +0000

    tagline e blablassqwqdq

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://tppi89975.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/dokku/',

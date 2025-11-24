2e9dfaa979b917f68ab0253edbcc75dc87e6decf
Martim Gomes <a89975@ualg.pt>
2025-11-24 15:17:44 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/tppi89975/dokku.git',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/dokku/',
